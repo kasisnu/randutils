a03f4e93965cb26005b864c4b8672dec8355eec0
kasisnu <kasisnu@gmail.com>
2016-05-03 13:10:09 -0400

    first commit

---
+++ ips/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+// http://stackoverflow.com/questions/10006459/regular-expression-for-ip-address-validation
+var IP_REGEX = regexp.MustCompile(`(([1-9]?\d|1\d\d|2[0-5][0-5]|2[0-4]\d)\.){3}(1\d\d|2[0-5][0-5]|2[0-4]\d|[1-9]?\d)`)
+
+func main() {
+	var results = []string{}
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		input := s.Text()
+		matches := IP_REGEX.FindAllString(input, -1)
+		results = append(results, matches...)
+	}
+
+	for _, ip := range results {
+		fmt.Println(ip)
+	}
+}
